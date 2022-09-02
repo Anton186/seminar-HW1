666e3abee265011212ff6e349a93b75eadb08744
Чернышев Антон <black2604@mail.ru>
2022-09-02 12:44:47 +0500

    сделал задачу 6

--- zadacha6/Program.cs
+++ zadacha6/Program.cs
@@ -3,3 +3,14 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Напишите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA %2 == 1)
+{
+    Console.WriteLine("Число не четное");
+}
+else 
+{
+    Console.WriteLine("Число четное");
+}
--- zadacha8/Program.cs
+++ zadacha8/Program.cs
@@ -2,3 +2,17 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Напишите число :");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (numberA < 1)
+{
+    Console.WriteLine("Тут нету четных чисел");
+}
+while (count < numberA -3)
+{
+    count = count + 2;
+    Console.Write(count + ", ");
+}
+
